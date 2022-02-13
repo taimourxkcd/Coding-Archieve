d46c9f4d5aa3b012304ae2de9ea7d08b75167d47
taimourz <taimour.rkt@gmail.com>
2022-02-13 16:07:31 +0500

    after a long time

---
+++ Programming/FirstWebNew/App/js/script.js
@@ -0,0 +1,12 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+  } else {
+    header.classList.add("open");
+  }
+});
